9fe0276088da537196508ea41b2e6c7311e80f5b
Krushnai jadhav <120713013+Krushnai2810@users.noreply.github.com>
2023-10-09 20:15:45 +0530

    Add files via upload

---
+++ MethodDemo.java
@@ -0,0 +1,11 @@
+public class MethodDemo{
+static void m1(){
+System.out.println("NIKITA");
+}
+public static void main(String args[]){
+System.out.println("NIkhil");
+m1();
+System.out.println("VIKAS");
+}
+}
+
